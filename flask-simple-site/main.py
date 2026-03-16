4fbc85a1c09a339d3a780dc4f204d8e32b83f278
Aron <lanzendoerfer.aron@gmail.com>
2026-03-16 11:26:38 +0100

    Funktion Main hinzugefügt

--- flask-simple-site/main.py
+++ flask-simple-site/main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 from management import *
  
+
 app = Flask(__name__)
  
 @app.route("/")
 def home():
     return render_template("startseite.html")
- 
+
+def main():
+    Datenbank()
+
 if __name__ == "__main__":
     app.run(debug=True)
+    main()
+    
